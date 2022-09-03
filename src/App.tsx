78acb2e7f38a888fc885228426d9f7e2e68176a8
Ebuka Odini <ebukaodini@gmail.com>
2022-09-03 14:39:25 +0100

    refactor: added the default page

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,23 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-center text-xl font-bold underline">
-      Hello, Welcome to Onyinye.ng!
-    </h1>
+    <div className='h-screen bg-sky-50'>
+      <div className='h-2/3 flex justify-center items-center'>
+
+        <div className='flex flex-col justify-center items-center w-1/4 gap-2 text-sky-800'>
+          <img src={logo} alt="Logo" className='w-1/3' />
+          
+          <h1 className='text-4xl font-semibold'>Onyinye Technologies</h1>
+          
+          <span className='text-opacity-90 text-center mt-3'>Onyinye is a business that manufactures and delivers Gift-Cards to businesses across Nigeria and Africa.</span>
+
+          <button className='p-3 px-8 rounded-md hover:opacity-90 mt-5 bg-sky-500 text-sky-50'>
+            Create a Demo Business
+          </button>
+        </div>
+
+      </div>
+    </div>
   );
 }
 
