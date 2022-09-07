2cc6a2b7b88a06e1ebc1c31089a3a56d4a2088e0
Ebuka Odini <ebukaodini@gmail.com>
2022-09-07 18:19:27 +0100

    fix: fixed frontend test bug

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,31 @@
-import React from "react"
+import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
-import App from "./App"
+import React from "react"
+import userEvent from "@testing-library/user-event"
+import "@testing-library/jest-dom"
+import { MemoryRouter } from "react-router-dom"
+import { createMemoryHistory } from "history"
+import App from "../App"
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Onyinye Technologies/i)
-  expect(linkElement).toBeInTheDocument()
+// Visit this for guidance
+// https://testing-library.com/docs/
+
+test("Testing route '/'", () => {
+  const history = createMemoryHistory()
+  const routes = ["/"]
+
+  // use <MemoryRouter> when you want to manually control the history
+  render(
+    <MemoryRouter initialEntries={routes}>
+      <App />
+    </MemoryRouter>
+  )
+
+  expect(screen.getByRole("heading").innerHTML).toBe("Onyinye Technologies")
+  expect(screen.getByText(/Create a Demo Business/i)).toBeInTheDocument()
+  expect(screen.getByText(/Log in to your Demo Business/i)).toBeInTheDocument()
+  expect(history.location.pathname).toEqual("/")
+
+  userEvent.click(screen.getByText(/Create a Demo Business/i))
+  expect(screen.getByText(/Register your business here.../i)).toBeInTheDocument()
 })
