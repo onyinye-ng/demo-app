968e9fc3dbd82ffe5bf589a97bd515d992a53fc1
Ebuka Odini <ebukaodini@gmail.com>
2022-09-07 05:30:44 +0100

    style: setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { Register, Welcome } from './pages'
+import { Suspense } from "react"
+import { Route, Routes } from "react-router-dom"
+import { Register, Welcome } from "./pages"
 
 function App() {
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-                    <Route path="/" element={<Welcome />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={<Welcome />}
+          />
+          <Route
+            path="/register"
+            element={<Register />}
+          />
         </Routes>
       </Suspense>
     </>
