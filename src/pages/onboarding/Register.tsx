5330bf2683bde3f5138fd27736b70db323da7645
Ebuka Odini <ebukaodini@gmail.com>
2022-09-08 11:13:21 +0100

    refactor: updated the register page

--- src/pages/onboarding/Register.tsx
+++ src/pages/onboarding/Register.tsx
@@ -1,60 +1,127 @@
-import React from "react"
+import React, { FormEvent, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { OnboardingWrapper } from "../../components"
+import { Button, CheckboxInput, Input, Label, TextButton } from "../../components/forms"
 
 export const Register: React.FC<{}> = () => {
+  const navigate = useNavigate()
+  const [isVerifying, setIsVerifying] = useState<boolean>(false)
+  const [credentials, setCredentials] = useState({
+    companyName: "",
+    email: "",
+    telephone: "",
+    token: "",
+  })
+
+  const handleChange = (field: string, value: string | boolean) => {
+    setCredentials({
+      ...credentials,
+      [field]: value,
+    })
+    if (field === "email") setIsVerifying(false)
+  }
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    if (isVerifying === false) {
+      setIsVerifying(true)
+      return
+    }
+
+    navigate("/dashboard")
+  }
+
   return (
     <OnboardingWrapper>
-      <div className="flex justify-center gap-10 text-primary-light">
-        <div className="flex flex-col border rounded-md px-6 py-10 gap-4 w-64 md:w-9/12 lg:min-w-fit backdrop-blur-xl bg-white/30 backdrop-brightness-95">
-          <div className="mt-2 text-center mb-2">
-            <h3 className="antialiased text-3xl font-semibold">Create Demo Business</h3>
-          </div>
-          <form className="flex flex-col gap-4">
-            <div>
-              <div className="pb-3">
-                <label htmlFor="companyName"> Company Name</label>
-              </div>
-              <div>
-                <input
-                  type="text"
-                  name="companyName"
-                  placeholder="Company Name"
-                  className="border rounded-md p-2 w-full bg-primary-light text-grey-dark"
-                />
-              </div>
+      <div className="h-3/4 w-5/6 md:w-6/12 md:min-w-fit lg:w-3/12 lg:min-w-fit mx-auto">
+        <div className="mt-14 flex flex-col gap-4 justify-start">
+          <h3 className="text-3xl w-full text-center">Create Business</h3>
+
+          <form
+            onSubmit={handleSubmit}
+            className="w-full h-full flex flex-col gap-3"
+          >
+            <div className="w-full flex flex-col gap-1">
+              <Label htmlFor="companyName">Company Name</Label>
+              <Input
+                onChange={(e) => handleChange("companyName", e.target.value)}
+                value={credentials.companyName}
+                id="companyName"
+                autoComplete="off"
+                placeholder="ex. XYZ Company"
+                required
+              />
             </div>
-            <div>
-              <div className="pb-3">
-                <label htmlFor="email"> Email Address </label>
-              </div>
-              <div>
-                <input
-                  type="text"
-                  name="email"
-                  placeholder="Email Address"
-                  className="border rounded-md p-2 w-full bg-primary-light text-grey-dark"
-                />
-              </div>
+
+            <div className="w-full flex flex-col gap-1">
+              <Label htmlFor="email">Business Email</Label>
+              <Input
+                onChange={(e) => handleChange("email", e.target.value)}
+                type="text"
+                value={credentials.email}
+                id="email"
+                autoComplete="email"
+                required
+                placeholder="ex. xyz.company@example.com"
+              />
             </div>
-            <div>
-              <div>
-                <input
-                  type="checkbox"
-                  name="suscribe"
-                />
-                <label htmlFor="suscribe"> Suscribe to our newsletter</label>
-              </div>
+
+            <div className="w-full flex flex-col gap-1">
+              <Label htmlFor="telephone">Business Telephone</Label>
+              <Input
+                onChange={(e) => handleChange("telephone", e.target.value)}
+                type="text"
+                value={credentials.telephone}
+                id="telephone"
+                autoComplete="tel"
+                required
+                placeholder="ex. +234"
+              />
             </div>
-            <div>
-              <div className="text-center mx-auto border rounded-full hover:bg-primary-dark active:bg-primary-dark focus:outline-none focus:ring focus:ring-blue-300">
-                <button
-                  type="submit"
-                  className="p-2 text-primary-light  "
+
+            <div className="my-2 w-full flex gap-1 items-center">
+              <CheckboxInput
+                type="checkbox"
+                required
+                id="suscribe"
+              />
+              <Label
+                htmlFor="suscribe"
+                className="m-0"
+              >
+                Subscribe to our newsletter
+              </Label>
+            </div>
+
+            {isVerifying && (
+              <div className="w-full flex flex-col gap-1">
+                <Label htmlFor="token">Verification Token</Label>
+                <Input
+                  onChange={(e) => handleChange("token", e.target.value)}
+                  maxLength={6}
+                  id="token"
+                  autoComplete="off"
+                  placeholder="123456"
+                  value={credentials.token}
+                />
+                <TextButton
+                  type="button"
+                  className="self-end"
+                  title="Resend code"
                 >
-                  Submit{" "}
-                </button>
+                  Resend Code
+                </TextButton>
               </div>
-            </div>
+            )}
+
+            <Button
+              title="Register"
+              type="submit"
+              className="mt-5 p-2 bg-secondary text-grey-dark"
+            >
+              {isVerifying === true ? "Register" : "Verify Email"}
+            </Button>
           </form>
         </div>
       </div>
