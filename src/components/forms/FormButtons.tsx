da0f02872545c5fad330ee9321422bbb8368f2ae
Ebuka Odini <ebukaodini@gmail.com>
2022-09-08 07:15:44 +0100

    feat: created form components

---
+++ src/components/forms/FormButtons.tsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom"
+
+/**
+ * An optimized button.
+ * Pass the `title` props, else `⚠` is returned
+ *
+ * @param props
+ * @returns button
+ */
+export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
+  if (!props["title"]) return <span children={"⚠"} />
+  return (
+    <button
+      aria-label={props.title}
+      {...props}
+      className={`p-3 px-6 text-center rounded-md focus:outline-none focus:shadow-none hover:opacity-90 active:opacity-90 ${props.className}`}
+    />
+  )
+}
+
+/**
+ * An optimized text button.
+ * Pass the `title` props, else `⚠` is returned
+ *
+ * @param props
+ * @returns button
+ */
+export const TextButton: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
+  if (!props["title"]) return <span children={"⚠"} />
+  return (
+    <button
+      aria-label={props.title}
+      {...props}
+      className={`p-0 focus:outline-none focus:shadow-none focus:underline active:underline ${props.className}`}
+    />
+  )
+}
+
+/**
+ * An optimized icon button.
+ * Pass the `title` props, else `⚠` is returned
+ *
+ * @param props
+ * @returns button
+ */
+export const IconButton: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
+  if (!props["title"]) return <span children={"⚠"} />
+  return (
+    <button
+      aria-label={props.title}
+      {...props}
+      className={`p-2 flex justify-center items-center rounded-full focus:outline-none focus:shadow-none hover:opacity-90 active:opacity-90 ${props.className}`}
+    />
+  )
+}
+
+type LinkProps = {
+  children: any
+  title: string
+  className?: string
+  to: string
+}
+
+/**
+ * An optimized link button.
+ * Pass the `title` props, else `⚠` is returned
+ *
+ * @param props
+ * @returns button
+ */
+export const LinkButton: React.FC<LinkProps & React.RefAttributes<HTMLAnchorElement>> = (props) => {
+  if (!props["title"]) return <span children={"⚠"} />
+  return (
+    <Link
+      aria-label={props.title}
+      {...props}
+      className={`p-3 px-6 text-center rounded-md focus:outline-none focus:shadow-none hover:opacity-90 active:opacity-90 ${props.className}`}
+    />
+  )
+}
---
+++ src/components/forms/index.tsx
@@ -0,0 +1,3 @@
+export { Button, IconButton, LinkButton, TextButton } from "./FormButtons"
+export { IconLink, TextLink } from "./Links"
+export { Input, CheckboxInput, TextareaInput, Label } from "./Inputs"
