9f098af001376bb676a6f128b124936ddf73c988
Bruno_odinukweze <brunofreelancing@gmail.com>
2022-09-08 21:38:51 +0100

    Revert "Revert "feat: created cards page""
    This reverts commit ff8cabdd83472c87ce19842b1aa4684945d24115.

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -128,6 +128,7 @@ export const Dashboard: React.FC<{}> = () => {
             <li>
               <a
                 href="."
+                aria-current="page"
                 className="py-2 px-3 leading-tight text-gray-500 bg-white border-inherit border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               >
                 1
@@ -144,7 +145,6 @@ export const Dashboard: React.FC<{}> = () => {
             <li>
               <a
                 href="."
-                aria-current="page"
                 className="py-2 px-3 text-blue-600 bg-blue-50 border-inherit border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
               >
                 3
