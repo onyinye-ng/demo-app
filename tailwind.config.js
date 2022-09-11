3790af4f70c93225289811e007577140430d9110
Bruno_odinukweze <brunofreelancing@gmail.com>
2022-09-11 14:25:42 +0100

    feat: created card details page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require("tailwindcss/colors")
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
   theme: {
     colors: {
       primary: "#005f94",
