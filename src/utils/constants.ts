f6c28973353584e15681d7921b70fa943b6fdcb9
Ebuka Odini <ebukaodini@gmail.com>
2022-09-13 14:11:02 +0100

    feat: added more utils functions

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,3 @@
 export const appId = process.env.REACT_APP_APP_ID
 export const domain = process.env.REACT_APP_DOMAIN
 export const apiDomain = process.env.REACT_APP_API_DOMAIN
-
-export const authToken = () => ({
-  Authorization:
-    "Bearer " + JSON.parse(localStorage.getItem(appId + ".account") ?? "{}")?.state?.authToken ??
-    "",
-})
